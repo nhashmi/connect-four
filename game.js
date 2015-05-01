b9931d4bf39ea1939fe47fb65c2f76bdd74fc94d
Nabil Hashmi <nabil2011@gmail.com>
2015-05-01 16:24:37 -0400

    Clicking the column will drop a piece in the bottommost space

--- game.js
+++ game.js
@@ -54,13 +54,11 @@ $(document).ready(function(){
           for (var n = 0; n < classes.length; n++) {
             if (columnPattern.test(classes[n])){
               column = classes[n].slice(-1);
-              console.log("Column is: " + column);
             }
           }
           for (var k = 0; k < parentClasses.length; k++) {
             if (rowPattern.test(parentClasses[k])){
               row = parentClasses[k].slice(-1);
-              console.log("Row is: " + row);
             }
           };
           // Find piece in the same column with clickable row - 1
@@ -68,7 +66,64 @@ $(document).ready(function(){
           $(nextPieceUp).addClass("clickable");
           // Assign clickable to that piece  
         } else if (!$(this).hasClass("clicked")) {
-          alert ("You can't click there yet!")
+            alert("You can't click there yet!");
+            // Iterate through column
+            var thisColumn = this.classList[2];
+            var clickableInColumn = $('.' + thisColumn);
+            for (var i = 0; i < clickableInColumn.length; i++) {
+              piece = $(clickableInColumn[i])
+              if (piece.hasClass("clickable") && redTurn) {
+                console.log("red!")
+                $(piece).addClass("clicked");
+                $(piece).removeClass("clickable");
+                $(piece).css("background", "red");
+                // Determine column 
+                var classes = $(piece)[0].classList;
+                var parentClasses = $(piece).parent()[0].classList;
+                var rowPattern = /^row-/;
+                var columnPattern = /^column-/;
+                for (var n = 0; n < classes.length; n++) {
+                  if (columnPattern.test(classes[n])){
+                    column = classes[n].slice(-1);
+                  }
+                }
+                for (var k = 0; k < parentClasses.length; k++) {
+                  if (rowPattern.test(parentClasses[k])){
+                    row = parentClasses[k].slice(-1);
+                  }
+                };
+                // Find piece in the same column with clickable row - 1
+                var nextPieceUp = $(".row-" + (row - 1)).children()[column - 1];
+                $(nextPieceUp).addClass("clickable");
+                // Assign clickable to that piece
+                redTurn = false
+              } else if (piece.hasClass("clickable") && !redTurn){
+                console.log("black!")
+                $(piece).addClass("clicked");
+                $(piece).removeClass("clickable");
+                $(piece).css("background", "#000");
+                // Determine column 
+                var classes = $(piece)[0].classList;
+                var parentClasses = $(piece).parent()[0].classList;
+                var rowPattern = /^row-/;
+                var columnPattern = /^column-/;
+                for (var n = 0; n < classes.length; n++) {
+                  if (columnPattern.test(classes[n])){
+                    column = classes[n].slice(-1);
+                  }
+                }
+                for (var k = 0; k < parentClasses.length; k++) {
+                  if (rowPattern.test(parentClasses[k])){
+                    row = parentClasses[k].slice(-1);
+                  }
+                };
+                // Find piece in the same column with clickable row - 1
+                var nextPieceUp = $(".row-" + (row - 1)).children()[column - 1];
+                $(nextPieceUp).addClass("clickable");
+                // Assign clickable to that piece
+                redTurn = true
+              };
+            }            
         } else {
           alert("That piece has already been clicked!");
         }
